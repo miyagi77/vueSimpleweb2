a29f23d3bac7ad4b5e730c137f29c42dc21f8112
miyagi <zhanglei@infoearth.com>
2018-12-18 18:25:08 +0800

    添加样式

--- vue-simple/src/components/head.vue
+++ vue-simple/src/components/head.vue
@@ -7,6 +7,11 @@
                 {{item.name}}
             </router-link>
         </li>
+        <div style="float:right">
+            <span><i class="el-icon-search" style="position: absolute;top: 20px;left: 60.2%;"></i></span><input type="text" placeholder="搜索" style="height: 30px;margin: 4px;padding-left: 20px;">
+            <el-button icon="el-icon-mobile-phone">登陆</el-button>
+            <el-button icon="el-icon-edit">注册</el-button>
+        </div>
       </ul>
   </div>
 </template>
@@ -15,8 +20,7 @@
 export default {
   data() {
     return {
-      activeName: "首页",
-       isActive:0
+      isActive: 0
     };
   },
   computed: {
@@ -26,15 +30,17 @@ export default {
   },
   methods: {
     //   鼠标移入
-      mouseEnter(index){
-        this.isActive = index;
-      },
+    mouseEnter(index) {
+      this.isActive = index;
+    },
     //   鼠标移除
-      mouseLeave(){
-          this.isActive=null;
-      },
+    mouseLeave() {
+      this.isActive = null;
+    }
   },
-  mounted() {}
+  mounted() {
+      
+  }
 };
 </script>
 
@@ -66,6 +72,9 @@ export default {
 .active {
   background-color: #e7e7e7;
   color: #ffb86c !important;
+}
+.search {
+  position: relative;
 }
 </style>
 
--- vue-simple/src/main.js
+++ vue-simple/src/main.js
@@ -3,12 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Elementui from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
+Vue.use(Elementui)
 
 new Vue({
   router,
   store,
-  Elementui,
   render: h => h(App),
 }).$mount('#app')
